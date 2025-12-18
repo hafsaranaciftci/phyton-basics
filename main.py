aae318c2a71d09cb2126a5e090b6816c7028d73c
hafsaranaciftci <hafsaranaciftci@gmail.com>
2025-12-18 13:04:54 +0300

    toplama programi eklendi

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello GitHub ! Python öğreniyorum."
+sayi1 = int(input("Birinci sayı gir:"))
+sayi2 = int(input("İkinci sayıyı gir:"))
+
+toplam = sayi1 + sayi2
+
+print("Toplam:", toplam)
+         
